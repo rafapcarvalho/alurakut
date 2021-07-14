3361db0a3463b60acf0ffaab28cfd21459c38f90
Rafael Carvalho <rafapcarvalho@gmail.com>
2021-07-13 23:23:33 -0300

    Primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import MainGrid from '../src/components/MainGrid/index'
+import Box from '../src/components/Box/index'
+import { AlurakutMenu } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+
+function ProfileSidebar(propriedades){
+  console.log(propriedades);
+  return (
+    <Box>
+    <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px'}}/>
+    </Box>
+  )
+}
+
+
+export default function Home() {
+  const usuarioAleatorio = 'rafapcarvalho';
+  const pessoasFavoritas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev', 'felipefialho']
+
+
+  return (
+    <>
+    <AlurakutMenu />
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <ProfileSidebar githubUser={usuarioAleatorio} />
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+      <Box>
+        Bem vindo
+      </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+      <ProfileRelationsBoxWrapper>
+        Pessoas da comunidade ({pessoasFavoritas.length})
+
+        <ul>
+          {pessoasFavoritas.map((itemAtual) => {
+            return (
+            <li>
+              <a href={`/users/${itemAtual}`} key={itemAtual}>
+                <img src={`https://github.com/${itemAtual}.png`} />
+                <span>{itemAtual}</span>
+              </a>
+            </li>    
+            )
+          })}
+        </ul>
+      </ProfileRelationsBoxWrapper>
+      <Box>
+        Comunidades
+      </Box>
+      </div>
+    </MainGrid>
+  </>
+  )
+}
